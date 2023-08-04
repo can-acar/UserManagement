ab7f3ab2fc8828beeaba18225777fd406e1f9644
Can Acar <can.acar2@ogr.sakarya.edu.tr>
2023-08-04 14:36:03 +0300

    1.0.8

--- UserManagement.Core/Services/IIdentityService.cs
+++ UserManagement.Core/Services/IIdentityService.cs
@@ -2,7 +2,7 @@ namespace UserManagement.Core.Services
 {
     public interface IIdentityService
     {
-        Task ForgotPassword(string email);
+        Task<ServiceResponse> ForgotPassword(string email);
 
 
         Task<ServiceResponse> Login(string username, string password);
